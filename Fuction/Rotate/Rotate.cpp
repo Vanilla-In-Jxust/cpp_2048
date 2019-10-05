4061a0f8c0254adbaf52d9a1ef3dd0e09c886466
VergeDX <wxy20001225@126.com>
2019-10-05 18:10:41 +0800

    simplify Rotate

--- Fuction/Rotate/Rotate.cpp
+++ Fuction/Rotate/Rotate.cpp
@@ -5,27 +5,11 @@ using namespace std;
 vector<int> rotateRight(const vector<int> &before) {
     vector<int> after;
 
-    // TODO: Simplify this!!!
-
-    after.push_back(before[12]);
-    after.push_back(before[8]);
-    after.push_back(before[4]);
-    after.push_back(before[0]);
-
-    after.push_back(before[13]);
-    after.push_back(before[9]);
-    after.push_back(before[5]);
-    after.push_back(before[1]);
-
-    after.push_back(before[14]);
-    after.push_back(before[10]);
-    after.push_back(before[6]);
-    after.push_back(before[2]);
-
-    after.push_back(before[15]);
-    after.push_back(before[11]);
-    after.push_back(before[7]);
-    after.push_back(before[3]);
+    for (int i = 12; i < 16; i++) {
+        for (int j = i; j > i - 13; j -= 4) {
+            after.push_back(before[j]);
+        }
+    }
 
     return after;
 }
